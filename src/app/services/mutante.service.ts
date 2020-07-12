7cf76d71e2379c7c136b2b0df83e3d07750b9b1f
guidorauber <guidomrauber@gmail.com>
2020-07-12 02:28:18 -0300

    first commit

--- src/app/services/mutante.service.ts
+++ src/app/services/mutante.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MutanteService {
-  API_URI = 'http://localhost:3000/';
+  API_URI = 'http://localhost:3000';
   constructor(private http: HttpClient) { }
 
   getGames() {
